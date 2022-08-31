8bb2327f01e09cb2b698915075780dc598b60dd6
Daria <kisichekk@gmail.com>
2022-08-31 13:19:15 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/mario_game_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mario_game_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        Expanded(
+            flex: 4,
+            child: Container(
+              color: Colors.blue,
+            )),
+        Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.brown,
+              child: Row(
+                children: [
+                  Container(
+                    color: Colors.brown[300],
+                    child: Icon(Icons.arrow_back),
+                  )
+                ],
+              ),
+            ))
+      ],
+    ));
+  }
+}
